65ff0cf122e2a7de2b6d421872b6cca951a42c30
4na7i1 <4na7i1@gmail.com>
2023-04-03 18:12:36 +0900

    maintenance

--- SECURITY/test_executeAdmin.ps1
+++ SECURITY/test_executeAdmin.ps1
@@ -1,8 +1,8 @@
+# test-file:module_executeAdmin.psm1
+
 # Import Module
 Import-Module -Name "$($PSScriptRoot)\module_sec\module_executeAdmin.psm1" -Force
 
 test $PSCommandPath $((Get-Location).Path)
-
-Write-Output "ABCD~"
 
 Pause
