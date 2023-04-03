18c924f2161aa9f8d4e2972483e8fa25e0206af3
4na7i1 <4na7i1@gmail.com>
2023-04-03 18:14:23 +0900

    add PsScripts [WEB]

---
+++ WEB/download_url.ps1
@@ -0,0 +1,19 @@
+# Proxy
+$proxy = "http://34.146.64.228:3128"
+
+# UserAgent
+$userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
+
+# Source URL
+$url = "https://github.com/about"
+
+# Destation file
+$dest = "$((Get-Location).Path)\test.html"
+
+# Download the file (Method1)
+Invoke-WebRequest -Uri $url -OutFile $dest -SkipCertificateCheck -Proxy $proxy -UserAgent $userAgent
+
+# # Download the file (Method2)
+# Start-BitsTransfer -Source $url -Destination $dest -Asynchronous -Priority normal
+# # Complete the BitsTransfer
+# Get-BitsTransfer | Complete-BitsTransfer
